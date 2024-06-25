787c2ed96e6d44af4f026c766f89b42891eb4efb
Nicolas Botta <127706026+NicolassBotta@users.noreply.github.com>
2024-06-25 17:51:11 -0300

    Delete AppCoder directory

--- AppCoder/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Avatar, curso, estudiante, profesor, entregable 
-# Register your models here.
-admin.site.register(curso)
-admin.site.register(estudiante)
-admin.site.register(profesor)
-admin.site.register(entregable)
-admin.site.register(Avatar)
